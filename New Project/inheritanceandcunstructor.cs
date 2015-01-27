3a435ec4607d619be7f7bcb7e4b009e293883ed4
Anil Chauhan <chauhan.anil033@gmail.com>
2015-01-27 21:08:11 +0530

    Update inheritanceandcunstructor.cs

--- New Project/inheritanceandcunstructor.cs
+++ New Project/inheritanceandcunstructor.cs
@@ -9,7 +9,7 @@ class Program
         {
             Console.WriteLine("Cunstructor in A");
         }
-        virtual public void display()
+         public virtual void display()
         {
             Console.WriteLine("display in A");
         }
@@ -20,30 +20,77 @@ class Program
         {
         Console.WriteLine("Cunstructor in B");
         }
-          override public void display()
+           public override void display()
         {
             Console.WriteLine("display in B");
         }
     }
+    
+    public class C:B
+    {
+        public C()
+        {
+        Console.WriteLine("Cunstructor in C");
+        }
+           public sealed override void display()
+        {
+            Console.WriteLine("display in C");
+        }
+    }
+    
+    public class D:C
+    {
+        public D()
+        {
+        Console.WriteLine("Cunstructor in D");
+        }
+           public new void display()
+        {
+            Console.WriteLine("display in D");
+        }
+    }
+    
     static void Main()
     {
         //Console.WriteLine("Hello, World!");
         A obja=new A();
         B objb=new B();
+        C objc=new C();
+        D objd=new D();
+        
         obja.display();
         objb.display();
-        A objm=new B();
+         objc.display();
+        objd.display();
+        
+        A objm=(A)objb;
         //B objmm=new A();//it gives error
        objm.display();
+        
+       
+        B objmm=(B)objc;
+        objmm.display();
+        
+        C objcmm=(C)objd;
+        objcmm.display();//sealed function or class can not be inherited, either by using override .
     }
 }
 /** note:private cunstructor can not be inherited**/
 /*output:
-Cunstructor in A                                                              
-Cunstructor in A                                                              
-Cunstructor in B                                                              
-display in A                                                                  
-display in B                                                                  
-Cunstructor in A                                                              
-Cunstructor in B                                                              
-display in B*/ 
+Cunstructor in A
+Cunstructor in A
+Cunstructor in B
+Cunstructor in A
+Cunstructor in B
+Cunstructor in C
+Cunstructor in A
+Cunstructor in B
+Cunstructor in C
+Cunstructor in D
+display in A
+display in B
+display in C
+display in D
+display in B
+display in C
+display in C*/ 
